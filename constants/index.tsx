15c4d9ccc095428cd03853ecb78444b345c2d941
Kim Junyoung <orsay0827@gmail.com>
2022-11-19 11:23:50 +0900

    deploy setting

--- constants/index.tsx
+++ constants/index.tsx
@@ -1,4 +1,7 @@
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8080'
+    : 'http://http://ec2-3-39-151-81.ap-northeast-2.compute.amazonaws.com';
 export const LOGIN_URL = API_BASE_URL + '/login';
 export const LOGOUT_URL = API_BASE_URL + '/logout';
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google';
